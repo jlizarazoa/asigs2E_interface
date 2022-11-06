29f72f87d18479def70e65c2833d98ab464a4262
jorodriguezal <72422459+jorodriguezal@users.noreply.github.com>
2022-11-06 18:51:16 -0500

    Error handling on 2D subject sequests

--- asignaturas_interface/routes/2D_subjects.js
+++ asignaturas_interface/routes/2D_subjects.js
@@ -4,12 +4,17 @@ let express = require("express");
 let router = express.Router();
 
 const request_2D_subjects = async (req, res) => {
-  console.log("Requesting 2D subjects...");
-  let client = await soap.createClientAsync(url);
-  console.log("Client created...");
-  let result = await client.AllSubjectsAsync({});
-  console.log("Result: ", result);
-  return result;
+  try {
+    console.log("Requesting 2D subjects...");
+    let client = await soap.createClientAsync(url);
+    console.log("Client created...");
+    let result = await client.AllSubjectsAsync({});
+    console.log("Result: ", result);
+    return result;
+  } catch (error) {
+    console.log("Error: ", error);
+    return error;
+  }
 };
 router.get("/", async (req, res) => {
   let result = await request_2D_subjects(req, res);
