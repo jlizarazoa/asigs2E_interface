c5615f4f8675d82f5e858020a18321ad58e41ef0
jorodriguezal <72422459+jorodriguezal@users.noreply.github.com>
2022-11-06 10:19:11 -0500

    AsignaturasService implemented

--- asignaturas_interface/app.js
+++ asignaturas_interface/app.js
@@ -1,12 +1,13 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 const CalculatorService = require("./services/calculatorService");
-var app = express();
+const AsignaturasService = require("./services/asignaturasService");
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -22,6 +23,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 app.use("/soap/calculation", CalculatorService);
+app.use("/soap/asignaturas", AsignaturasService);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
